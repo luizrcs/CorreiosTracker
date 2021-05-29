18d63faad1b06dd8be40bec6a8f5b08d2736dd71
luizrcs <luizrodrigo40@gmail.com>
2021-05-29 01:16:27 -0300

    [WIP] Implement 'add parcel' dialog

--- app/src/main/java/br/com/luizrcs/correiostracker/activity/MainActivity.kt
+++ app/src/main/java/br/com/luizrcs/correiostracker/activity/MainActivity.kt
@@ -1,11 +1,15 @@
 package br.com.luizrcs.correiostracker.activity
 
 import android.os.*
+import android.util.*
 import androidx.appcompat.app.*
+import androidx.appcompat.widget.*
+import androidx.core.content.res.*
 import androidx.navigation.fragment.*
 import androidx.navigation.ui.*
 import br.com.luizrcs.correiostracker.R
 import br.com.luizrcs.correiostracker.databinding.*
+import com.google.android.material.dialog.*
 import dagger.hilt.android.*
 
 @AndroidEntryPoint
@@ -23,5 +27,25 @@ class MainActivity: AppCompatActivity() {
 		
 		val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
 		binding.bottomNavigation.setupWithNavController(navHostFragment.navController)
+		
+		binding.fab.setOnClickListener {
+			MaterialAlertDialogBuilder(this)
+				.setView(R.layout.add_parcel_dialog)
+				.setBackground(ResourcesCompat.getDrawable(resources, R.drawable.dialog_shape, null))
+				.show()
+				.apply {
+					val dialogBinding = AddParcelDialogBinding.inflate(layoutInflater)
+					
+					dialogBinding.cancel.setOnClickListener {
+						Log.i("TEST", "cancel")
+						dismiss()
+					}
+					
+					dialogBinding.confirm.setOnClickListener {
+						Log.i("TEST", "confirm")
+						dismiss()
+					}
+				}
+		}
 	}
 }
